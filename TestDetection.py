fa5434a6a8066404da6c85d7e1ae9165f4d8647f
bpbdkotakediri <bpbd.kotakediri113@gmail.com>
2024-07-02 10:10:23 +0700

    perbaikan dataset terlebih dahulu

--- TestDetection.py
+++ TestDetection.py
@@ -1,35 +1,59 @@
 import cv2
+from ultralytics import YOLO
 
-# Menggunakan URL stream dengan parameter timeout
-url = "rtsp://admin:admin@192.168.1.37:8554/Streaming/Channels/101"  # timeout dalam milidetik
+# Path video file atau URL stream
+# video_source = 'rtsp://admin:admin@192.168.1.37:8554/Streaming/Channels/101'
+# video_source = 'D:\\KHOI\\PYTHON\\REF\\pengendara motor\\video\\Suasana Jalan Jatingaleh, Semarang saat jam kerja.mp4'
+source = "D:\\KHOI\\PYTHON\\REF\\pengendara motor\\Dataset Plat Nomor\\i25_screenshot-2023-11-30-153007_jpg.rf.f60ac55ffde67a27eb7cd042a9865577.jpg"
 
-cap = cv2.VideoCapture(0)
+# Membuka video
+cap = cv2.VideoCapture(source)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
 
+# Tentukan ukuran frame output yang diinginkan
+output_width = 640
+output_height = 480
+
+#model yolo
+model=YOLO("D:\\KHOI\\PYTHON\\Python_Face_Detection\\Detection.pt")
+
 if not cap.isOpened():
-    print("Error: Tidak dapat membuka stream.")
-    exit()
+    print("Error: Tidak dapat membuka video.")
+else:
+    while True:
+        ret, frame = cap.read()
 
-while True:
-    ret, frame = cap.read()
-    
-    if not ret:
-        print("Error: Tidak dapat membaca frame dari stream.")
-        break
-    
-    if frame is None or frame.size == 0:
-        print("Error: Frame kosong.")
-        continue
+        if not ret:
+            print("Error: Tidak dapat membaca frame dari stream.")
+            break
 
-    # Ubah ukuran frame
-    resized_frame = cv2.resize(frame, (640, 320))
+        if frame is None or frame.size == 0:
+            print("Error: Frame kosong.")
+            continue
+        
+        results = model(frame)
+        # Dapatkan hasil deteksi
+        for result in results:
+            for box in result.boxes.xyxy:
+                x1, y1, x2, y2 = map(int, box)
+                # Gambarkan kotak bounding box pada frame
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-    # Tampilkan frame yang diubah ukurannya
-    cv2.imshow('Kamera', resized_frame)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Ubah ukuran frame
+        resized_frame = cv2.resize(frame, (output_width, output_height))
 
-cap.release()
-cv2.destroyAllWindows()
+        # Tampilkan frame yang diubah ukurannya
+        cv2.imshow('Kamera CCTV', resized_frame)
 
+        # Tekan 'q' untuk keluar dari loop
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+            # break
+
+        # Tekan 'q' untuk keluar dari loop
+        if cv2.waitKey(10000) & 0xFF == ord('q'):
+            break
+
+
+    # Release semua resource yang digunakan
+    cap.release()
+    cv2.destroyAllWindows()
--- TestOcr.py
+++ TestOcr.py
@@ -3,18 +3,19 @@ from ultralytics import YOLO
 
 # Path video file atau URL stream
 # video_source = 'rtsp://admin:admin@192.168.1.37:8554/Streaming/Channels/101'
-video_source = 'D:\\KHOI\\PYTHON\\Source\\pengendara motor\\Dataset Plat Nomor\\82_jpg.rf.a242fdbe999b85787a42a40055cb4d21.jpg'
+# video_source = 'D:\\KHOI\\PYTHON\\REF\\pengendara motor\\video\\Suasana Jalan Jatingaleh, Semarang saat jam kerja.mp4'
+source = "D:\\KHOI\\PYTHON\\REF\\pengendara motor\\video\\VID20240311151058.mp4"
 
-# Membuka video
-cap = cv2.VideoCapture(video_source)
+# Membuka video atau gambar
+cap = cv2.VideoCapture(source)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
 
 # Tentukan ukuran frame output yang diinginkan
 output_width = 640
 output_height = 480
 
-#model yolo
-model=YOLO("D:\\KHOI\PYTHON\\Coba Python\\Test Detection\\runs\\detect\\train\\weights\\best.pt")
+# Model YOLO
+model = YOLO("D:\\KHOI\\PYTHON\\Python_Face_Detection\\best.pt")
 
 if not cap.isOpened():
     print("Error: Tidak dapat membuka video.")
@@ -33,10 +34,17 @@ else:
         results = model(frame)
         # Dapatkan hasil deteksi
         for result in results:
-            for box in result.boxes.xyxy:
-                x1, y1, x2, y2 = map(int, box)
+            for box in result.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                conf = box.conf[0]
+                cls = int(box.cls[0])
+                label = f'{model.names[cls]} {conf:.2f}'
+                
                 # Gambarkan kotak bounding box pada frame
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                
+                # Tambahkan label pada bounding box
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 2)
 
         # Ubah ukuran frame
         resized_frame = cv2.resize(frame, (output_width, output_height))
@@ -45,13 +53,8 @@ else:
         cv2.imshow('Kamera CCTV', resized_frame)
 
         # Tekan 'q' untuk keluar dari loop
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-            # break
-
-        # Tekan 'q' untuk keluar dari loop
-        if cv2.waitKey(10000) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
 
     # Release semua resource yang digunakan
     cap.release()
