d5288f290eb558f6f12174286f15fec487a375e2
Khoirul666 <khoirulrizqi1999@gmail.com>
2024-08-08 06:42:05 +0700

    coba train epoch 500

--- Training.py
+++ Training.py
@@ -3,8 +3,18 @@ from ultralytics import YOLO
 # Muat model pralatih YOLOv8
 model = YOLO('yolov8n.pt')  # atau model pralatih lainnya
 
-# Latih model menggunakan dataset Anda
-model.train(data=r'D:\KHOI\PYTHON\dataset\Full Plat Pengendara.v4i.yolov8\data.yaml', epochs=10, imgsz=640)
+# Konfigurasi pelatihan
+train_cfg = {
+    'data': r'D:\KHOI\PYTHON\dataset\Detection Plat.v3i.yolov8\data.yaml',  # Ganti dengan path ke file data.yaml Anda
+    'epochs': 500,  # Set jumlah epoch menjadi 500
+    'imgsz': 640,  # Ukuran gambar (ubah sesuai kebutuhan)
+    'batch': 16,  # Ukuran batch (ubah sesuai kebutuhan)
+    'patience': 0,  # Set nilai patience untuk EarlyStopping
+    'device': 'cpu',  # Ubah sesuai perangkat Anda ('0' untuk GPU, 'cpu' untuk CPU)
+}
+
+# Mulai pelatihan
+model.train(**train_cfg)
 
 # Simpan model yang telah dilatih
-model.save('DeteksiPengendara20240718.pt')
+model.save('DeteksiPengendara20240805.pt')
